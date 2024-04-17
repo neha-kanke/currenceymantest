41ec97f9e3916f52d31b55c7af93d30271efd71f
neha-kanke <nehakanke95@gmail.com>
2024-04-17 13:14:45 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,32 +19,62 @@ export class AppComponent implements OnInit {
       .subscribe((res: any) => {
         this.currencyarr = res
       })
+    this.basecodefunction()
+    this.targetcodefunction()
+
+
+
+
   }
+  basecodefunction() {
+    this.f['base_code1'].valueChanges
+      .subscribe(res => {
+        if (this.f['base_code1'].invalid) {
+          this.f['target_code'].disable()
+        } else {
+          this.f['target_code'].enable()
+        }
+      })
+  }
+  targetcodefunction() {
 
-
+    this.f['target_code'].valueChanges
+      .subscribe(res => {
+        if (this.f['target_code'].invalid) {
+          this.f['amount'].disable()
+        } else {
+          this.f['amount'].enable()
+        }
+      })
+  }
   createcuurnecyform() {
     this.currencyform = new FormGroup({
       base_code1: new FormControl(null, [Validators.required]),
-      amount: new FormControl(null, [Validators.required]),
-      target_code: new FormControl(null, [Validators.required]),
+      amount: new FormControl({ value: null, disabled: true }, [Validators.required]),
+      target_code: new FormControl({ value: null, disabled: true }, [Validators.required]),
       convertcurrey: new FormControl(null, [Validators.required]),
-
     })
+  }
+
+  get f() {
+    return this.currencyform.controls
   }
 
   oncuurntvalu() {
     console.log(this.currencyform.value);
-    let newobj = this.currencyform.value
-    this.currencyservice.getexchagecurrency(newobj)
-      .subscribe((res: any) => {
-        console.log(res.conversion_result);
-        this.covertcurray = res.conversion_result
-        // this.currencyform.patchValue({convertcurrey:res.conversion_rates})
-        // this.currencyform.controls['convertcurrey'].setValue(res.conversion_rates);
+    if (this.currencyform.valid) {
+      let newobj = this.currencyform.value
+      this.currencyservice.getexchagecurrency(newobj)
+        .subscribe((res: any) => {
+          console.log(res.conversion_result);
+          this.covertcurray = res.conversion_result
+          // this.currencyform.patchValue({convertcurrey:res.conversion_rates})
+          // this.currencyform.controls['convertcurrey'].setValue(res.conversion_rates);
 
 
-      })
+        })
 
+    }
 
 
 
